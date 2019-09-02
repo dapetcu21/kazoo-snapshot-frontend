e29e73e94656a4fd549bc754042042d0e74e43d0
Marius Petcu <dapetcu21@gmail.com>
2019-09-03 00:28:40 +0300

    Clarify GDPR wording

--- src/GDPRNotice.js
+++ src/GDPRNotice.js
@@ -8,8 +8,8 @@ export default function GDPRNotice({ onAccept }) {
       <div className="gdprText">
         <h1>Ge De Pe Re</h1>
         <p>Dacă ne dai poze, îți asumi faptul că ai dreptul să ni le dai si că nu îți e rușine cu ele.</p>
-        <p>Vom folosi pozele doar în timpul spectacolului pentru amuzamentul tău sau ca parte din înregistrări sau poze făcute spectacolului, pentru promovare.</p>
-        <p>Vom șterge pozele în maxim o zi după spectacol.</p>
+        <p>Vom folosi pozele doar în timpul spectacolului pentru amuzamentul tău sau ca parte din înregistrări sau poze făcute în timpul spectacolului, pentru promovare.</p>
+        <p>Vom șterge pozele tale în maxim o zi după spectacol.</p>
       </div>
       <button className="gdprButton" onClick={onAccept}>Da, da, am înțeles!</button>
     </div>
