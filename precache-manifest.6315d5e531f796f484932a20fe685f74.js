23036e234be34995adb0fe375372487e0066f8e0
Marius Petcu <dapetcu21@gmail.com>
2019-09-04 01:42:31 +0300

    Updates

---
+++ precache-manifest.6315d5e531f796f484932a20fe685f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0cace546acc6faa713345b96c4493b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "338cc9fecf20c8928c41",
+    "url": "/static/css/main.382b35e8.chunk.css"
+  },
+  {
+    "revision": "5e8858061eb16ddae218",
+    "url": "/static/js/2.8f671a61.chunk.js"
+  },
+  {
+    "revision": "338cc9fecf20c8928c41",
+    "url": "/static/js/main.a07eaef4.chunk.js"
+  },
+  {
+    "revision": "e44a276d9fa9cd34a333",
+    "url": "/static/js/runtime~main.9ba977f9.js"
+  },
+  {
+    "revision": "5f2e2fc51cb6d86624f91081bdee7f93",
+    "url": "/static/media/strip.5f2e2fc5.png"
+  }
+]);
